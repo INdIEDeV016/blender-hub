ca96f8c55bf9378185c21c6fc7423a07b530d402
Krishnendu Mondal <krishnendu.bhilai@gmail.com>
2026-01-16 01:09:44 +0530

    Theme changes and other minor changes
    Overhauled the look of the app in dark mode with a few minor changes here and there.

--- scenes/base_classes/page.gd
+++ scenes/base_classes/page.gd
@@ -10,7 +10,7 @@ var table_sections: Dictionary[StringName, TableSection] = {}
 
 func _ready() -> void:
 	%Title.tooltip_text = %Title.text
-	%SubText.tooltip_text = %SubText.text
+	%SubTitle.tooltip_text = %SubTitle.text
 
 	if not Engine.is_editor_hint():
 		get_tree().current_scene.pages.append(self)
